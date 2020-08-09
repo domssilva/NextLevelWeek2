a5c25399b102df904868c2a64be27f37badbb1a9
domssilva <silva@programmer.net>
2020-08-09 12:10:56 +0200

    [ExtraMile 2] - create faker utility to generate users

---
+++ back/src/controllers/TeacherController.ts
@@ -0,0 +1,14 @@
+import { Request, Response } from 'express';
+
+import generateUsers from '../utils/generateUsers';
+
+export default class ClassesController {
+  async index(request: Request, response: Response) {
+        let fakeUsers = [];
+        for (let i = 0; i < 5; i++) {
+            fakeUsers.push(generateUsers());
+        }
+
+        response.send(fakeUsers);
+    }
+}
--- back/src/routes.ts
+++ back/src/routes.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import ClassesController from './controllers/ClassesController';
 import ConnectionsController from './controllers/ConnectionsController';
+import TeacherController from './controllers/TeacherController';
 
 const routes = express.Router();
 const classesControllers = new ClassesController();
 const connectionsController = new ConnectionsController();
+const teacherController = new TeacherController();
 
 routes.get('/classes', classesControllers.index);
 routes.post('/classes', classesControllers.create);
@@ -13,5 +15,6 @@ routes.get('/connections', connectionsController.index);
 routes.post('/connections', connectionsController.create);
 
 // ExtraMile3: teacher route
+routes.get('/teacher', teacherController.index);
 
 export default routes;
