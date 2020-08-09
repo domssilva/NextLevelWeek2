880c03c219310790afc4c6d39f81c82a22dd409c
domssilva <silva@programmer.net>
2020-08-09 14:20:53 +0200

    [ExtraMile 3] - Create new route to fetch all registered teachers

--- back/src/controllers/TeacherController.ts
+++ back/src/controllers/TeacherController.ts
@@ -1,14 +1,17 @@
 import { Request, Response } from 'express';
-
-import generateUsers from '../utils/createFakeUser';
+import db from '../database/connection';
 
 export default class ClassesController {
-  async index(request: Request, response: Response) {
-        let fakeUsers = [];
-        for (let i = 0; i < 5; i++) {
-            fakeUsers.push(generateUsers());
-        }
+    async index(request: Request, response: Response) {
+        // returns ALL registered teachers
 
-        response.send(fakeUsers);
+        try {
+            const teachers = await db('users').select('*');
+            response.send(teachers);
+        } catch(error) {
+            response.send({
+                message: 'Cannot get teachers.'
+            });
+        }
     }
 }
--- back/src/routes.ts
+++ back/src/routes.ts
@@ -4,17 +4,20 @@ import ConnectionsController from './controllers/ConnectionsController';
 import TeacherController from './controllers/TeacherController';
 
 const routes = express.Router();
+
+// Controllers
 const classesControllers = new ClassesController();
 const connectionsController = new ConnectionsController();
 const teacherController = new TeacherController();
 
+// Routes
 routes.get('/classes', classesControllers.index);
 routes.post('/classes', classesControllers.create);
 
 routes.get('/connections', connectionsController.index);
 routes.post('/connections', connectionsController.create);
 
-// ExtraMile3: teacher route
-routes.get('/teacher', teacherController.index);
+// ExtraMile3: teachers route
+routes.get('/teachers', teacherController.index);
 
 export default routes;
